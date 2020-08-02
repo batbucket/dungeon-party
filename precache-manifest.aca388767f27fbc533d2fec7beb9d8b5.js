614a702d710264e2f19f753486b399eef7fa51d9
batbucket <andy.x.3030@gmail.com>
2020-08-02 04:42:48 -0400

    Updates

---
+++ precache-manifest.aca388767f27fbc533d2fec7beb9d8b5.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe85f68e0a3626cd31ba00f9bb94b2b",
+    "url": "/dungeon-party/index.html"
+  },
+  {
+    "revision": "ab1a711399cdd3c2318b",
+    "url": "/dungeon-party/static/css/2.4eb3805b.chunk.css"
+  },
+  {
+    "revision": "66528fae265fd9db4d05",
+    "url": "/dungeon-party/static/css/main.ce5fa41f.chunk.css"
+  },
+  {
+    "revision": "ab1a711399cdd3c2318b",
+    "url": "/dungeon-party/static/js/2.5982b06f.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/dungeon-party/static/js/2.5982b06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66528fae265fd9db4d05",
+    "url": "/dungeon-party/static/js/main.794df869.chunk.js"
+  },
+  {
+    "revision": "2f480f47b46b639aaf98",
+    "url": "/dungeon-party/static/js/runtime-main.dbbc24c5.js"
+  },
+  {
+    "revision": "ea9ed37af3e95dd1629a9581ceb2c4ed",
+    "url": "/dungeon-party/static/media/Itim-Regular.ea9ed37a.ttf"
+  },
+  {
+    "revision": "a7874ad604b7279f36fc799bdf34e395",
+    "url": "/dungeon-party/static/media/barbute.a7874ad6.svg"
+  },
+  {
+    "revision": "d874c95c1313acafe20f6bf29d939b25",
+    "url": "/dungeon-party/static/media/battle-gear.d874c95c.svg"
+  },
+  {
+    "revision": "239fa79895000a5282c4e21fa61ac9d8",
+    "url": "/dungeon-party/static/media/boiling-bubbles.239fa798.svg"
+  },
+  {
+    "revision": "f6e8b63c643add71dc187b1f745496f6",
+    "url": "/dungeon-party/static/media/bolt.f6e8b63c.svg"
+  },
+  {
+    "revision": "0f2ed5a2efb82ab1b1ef24f335c13fe2",
+    "url": "/dungeon-party/static/media/burning-dot.0f2ed5a2.svg"
+  },
+  {
+    "revision": "efc36af486130f1ffae79701611b718f",
+    "url": "/dungeon-party/static/media/cancel.efc36af4.svg"
+  },
+  {
+    "revision": "5ebc85c9306353fb9048636ba229a41c",
+    "url": "/dungeon-party/static/media/checked-shield.5ebc85c9.svg"
+  },
+  {
+    "revision": "a96fc1a2b03e4228829b8488317ab5f0",
+    "url": "/dungeon-party/static/media/clover.a96fc1a2.svg"
+  },
+  {
+    "revision": "5ca91fa948cf94a40fd9b3112f76460a",
+    "url": "/dungeon-party/static/media/cog.5ca91fa9.svg"
+  },
+  {
+    "revision": "9451b15359a8f187465ea57a6fd572f6",
+    "url": "/dungeon-party/static/media/confirmed.9451b153.svg"
+  },
+  {
+    "revision": "9f8fac980645ade419d8cfcbe39ca949",
+    "url": "/dungeon-party/static/media/crossed-swords.9f8fac98.svg"
+  },
+  {
+    "revision": "13f09ffed6ea04f63b5eb4371ea900ee",
+    "url": "/dungeon-party/static/media/crosshair.13f09ffe.svg"
+  },
+  {
+    "revision": "2f2226fe16541c01390aab58a1b574f0",
+    "url": "/dungeon-party/static/media/drop.2f2226fe.svg"
+  },
+  {
+    "revision": "2affc6f4824f02dc20193f07d868e501",
+    "url": "/dungeon-party/static/media/ember-shot.2affc6f4.svg"
+  },
+  {
+    "revision": "9a6122bdac01719085e3dfe124f3a8ce",
+    "url": "/dungeon-party/static/media/empty-chessboard.9a6122bd.svg"
+  },
+  {
+    "revision": "2d5d5f11f2f91f70cf109ee99a898028",
+    "url": "/dungeon-party/static/media/exit-door.2d5d5f11.svg"
+  },
+  {
+    "revision": "e13fd3466efbb37ef7ec93d67dd4eff4",
+    "url": "/dungeon-party/static/media/fireball.e13fd346.svg"
+  },
+  {
+    "revision": "81b50fa329aaccf9a7e4177d94bec9eb",
+    "url": "/dungeon-party/static/media/fist.81b50fa3.svg"
+  },
+  {
+    "revision": "d0b4c78f503850b0f1050e45a8ddb173",
+    "url": "/dungeon-party/static/media/gem-pendant.d0b4c78f.svg"
+  },
+  {
+    "revision": "d239747722da42318f777edb7ad59746",
+    "url": "/dungeon-party/static/media/heart-armor.d2397477.svg"
+  },
+  {
+    "revision": "e20dbb500da9d5d8a770dac66880120b",
+    "url": "/dungeon-party/static/media/heart.e20dbb50.svg"
+  },
+  {
+    "revision": "ca9034fb9a07851c3b4e0404638ed945",
+    "url": "/dungeon-party/static/media/histogram.ca9034fb.svg"
+  },
+  {
+    "revision": "d274bd9522150bc9fa706741c26ffed7",
+    "url": "/dungeon-party/static/media/hourglass.d274bd95.svg"
+  },
+  {
+    "revision": "4b8b4117bb4a3e67732cf4b774368442",
+    "url": "/dungeon-party/static/media/magic-shield.4b8b4117.svg"
+  },
+  {
+    "revision": "6f5cf37e738f2b66f6ef71a7f0c10e4b",
+    "url": "/dungeon-party/static/media/mail-shirt.6f5cf37e.svg"
+  },
+  {
+    "revision": "5d10da0cc6901c9ae9b01b5ccc022dff",
+    "url": "/dungeon-party/static/media/perspective-dice-six-faces-random.5d10da0c.svg"
+  },
+  {
+    "revision": "7a60a9cb02f373fdcdc71e2424825357",
+    "url": "/dungeon-party/static/media/plain-dagger.7a60a9cb.svg"
+  },
+  {
+    "revision": "a768e453779d2a6463a44c9d2af43c58",
+    "url": "/dungeon-party/static/media/pointy-sword.a768e453.svg"
+  },
+  {
+    "revision": "9ca39734c3ccee0f3b3689514cc8504a",
+    "url": "/dungeon-party/static/media/portal.9ca39734.svg"
+  },
+  {
+    "revision": "a325de95a3053b7d19e8326d54c0628f",
+    "url": "/dungeon-party/static/media/potion-ball.a325de95.svg"
+  },
+  {
+    "revision": "31634ae0a0c1c5bce683aff3933f0256",
+    "url": "/dungeon-party/static/media/return-arrow.31634ae0.svg"
+  },
+  {
+    "revision": "80fdf001f77aa58997f7c84b64a3e913",
+    "url": "/dungeon-party/static/media/robe.80fdf001.svg"
+  },
+  {
+    "revision": "58edd9827c8eb6e067c8c0ae2dd491c6",
+    "url": "/dungeon-party/static/media/rolling-dices.58edd982.svg"
+  },
+  {
+    "revision": "e2ab881a37baaa7f3b45057c2d3eb3e4",
+    "url": "/dungeon-party/static/media/sands-of-time.e2ab881a.svg"
+  },
+  {
+    "revision": "b8f0e09665accc00082902119992cf09",
+    "url": "/dungeon-party/static/media/shopping-cart.b8f0e096.svg"
+  },
+  {
+    "revision": "ae45baa9bfb545c7825288c5d2aaf1d1",
+    "url": "/dungeon-party/static/media/skills.ae45baa9.svg"
+  },
+  {
+    "revision": "4b1cdafde2ab1c89a45fcce9209ac0db",
+    "url": "/dungeon-party/static/media/spell-book.4b1cdafd.svg"
+  },
+  {
+    "revision": "a4c1275576a26bc40abf6066b2f2e53a",
+    "url": "/dungeon-party/static/media/unstable-orb.a4c12755.svg"
+  },
+  {
+    "revision": "7a346882f692b878c522a43fde0c1d2d",
+    "url": "/dungeon-party/static/media/upgrade.7a346882.svg"
+  },
+  {
+    "revision": "fbf93d2915d27ff559f3997a6e85a898",
+    "url": "/dungeon-party/static/media/wood-stick.fbf93d29.svg"
+  }
+]);
